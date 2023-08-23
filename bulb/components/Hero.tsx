6b3aa4da4dc40b18592924bfe7031ad4abd98330
mwahk3ymer <audreymwakima99@gmail.com>
2023-08-23 12:08:59 +0300

    bulb trial

---
+++ bulb/components/Hero.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+
+const Hero = () => {
+  return (
+    <div className="hero">
+        <div className="flex-1 pt-36 padding-x">
+           <h1 className="hero__title">
+            MY BULB
+           </h1>
+
+           <p className="hero__subtitle">Turn on and off
+           </p>
+
+           <CustomButton />
+        </div>
+      Hero
+    </div>
+  )
+}
+
+export default Hero
